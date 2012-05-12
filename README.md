e7e5b39189005cf5cfbf41d35cc802265510b0fe
Andrey Lushnikov <aslushnikov@gmail.com>
2012-05-12 13:10:38 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,75 @@
-readme.md-table-of-contents
-===========================
+# Table of Contents Generator
 
-Small and simple README.md preprocessor for creating table of contents 
+## About
+
+Small and simple README.md preprocessor for creating table of contents.
+
+Say you've got a README.md file with lots of information, and you want to
+create a table of contents with reference links
+
+This script will help you to do so. It extracts information of titles
+in a given file, and inserts a formatted table of contents in the position,
+specified by "@@TOC@@" line.
+
+## Usage
+
+```
+node toc.js README.md > NEW_README.md
+```
+
+## Example
+
+```
+# Foo Great Project
+
+Hey, this is my project
+
+## Contents
+@@TOC@@
+
+## About
+
+Some info about it
+
+## Authors
+
+My picture here
+```
+
+Will be transformed to
+```
+# Foo Great Project
+
+Hey, this is my project
+
+## Contents
+- [Foo Great Project](#foo-great-project)
+    - [Contents](#contents)
+    - [About](#about)
+    - [Authors](#authors)
+
+## About
+
+Some info about it
+
+## Authors
+
+My picture here
+```
+
+After the preprocessing you're free to modify the result as you wish.
+For example, it makes sense to remove reference to table of contents from
+table of contents
+
+## Limitations
+
+The script doesn't support underlined titles like this
+```
+My Title
+========
+```
+
+Use sharps instead
+```
+# My Title
+```
---
+++ toc.js
@@ -0,0 +1,64 @@
+var fs = require('fs');
+
+var FOUR_SPACES = "    ";
+
+var leftIndents = [""];
+
+for(var i = 1; i < 10; i++) {
+    leftIndents.push(leftIndents[i-1] + FOUR_SPACES);
+}
+
+fs.readFile(process.argv[2], 'utf-8', function(err, data) {
+    if (err) {
+        throw err;
+    }
+    processData(data);
+});
+
+function processData(data) {
+    var lines = data.trimRight().split('\n');
+
+    var titles = [];
+    var depths = [];
+    var minDepth = 1000000;
+    for(var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        var m = line.match(/^\s*(#+)(.*)$/);
+        if (!m) continue;
+        minDepth = Math.min(minDepth, m[1].length);
+        depths.push(m[1].length);
+        titles.push(m[2]);
+    }
+
+    for(var i = 0; i < depths.length; i++) {
+        depths[i] -= minDepth;
+    }
+
+    var toc = createTOC(depths, titles).join('\n');
+
+    var tocRegexp = /^\s*@@TOC@@\s*$/;
+    for(var i = 0; i <lines.length; i++) {
+        var line = lines[i];
+        if (tocRegexp.test(line)) {
+            lines[i] = toc;
+        }
+    }
+    console.log(lines.join('\n'));
+    //console.log('\n');
+}
+
+function createTOC(depths, titles) {
+    var ans = [];
+    for(var i = 0; i < depths.length; i++) {
+        ans.push(tocLine(depths[i], titles[i]));
+    }
+    return ans;
+}
+
+function titleToUrl(title) {
+    return title.trim().toLowerCase().replace(/\s/g, '-').replace(/[^-0-9a-z]/g, '');
+}
+
+function tocLine(depth, title) {
+    return leftIndents[depth] + "- [" + title.trim() + "](#" + titleToUrl(title) + ")";
+}
